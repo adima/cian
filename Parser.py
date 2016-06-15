599bdbe84b8bbc725a3ecff167b85b84af3b0a2c
adima <firtray@gmail.com>
2016-06-16 00:21:10 +0300

    Failsafe correction

--- Parser.py
+++ Parser.py
@@ -109,47 +109,51 @@ def parse_row(row):
 
 
 def process_district(driver, district, name):
-    final_list = []
-    print 'Processing %s' % name
-    init_url = 'http://www.cian.ru/cat.php?deal_type=sale' \
-               '&district%5B0%5D={}&engine_version=2&maxtarea=60&offer_type=flat&p=1&totime=86400'.format(district)
-    driver.get(init_url)
-    if district == districts.index[0]:
-        driver.find_element_by_xpath('//*[@id="layout"]/div[3]/div/div[2]/div/div[2]/a[1]').click()
-    result_n = int(
-        driver.find_element_by_class_name('serp-above__count').find_elements_by_tag_name('strong')[0].text)
-    print '%s results' % result_n
-    n_pages = np.ceil(result_n / float(25))
-    for page_n in range(1, int(n_pages) + 1):
+    try:
+        final_list = []
+        print 'Processing %s' % name
         init_url = 'http://www.cian.ru/cat.php?deal_type=sale' \
-                   '&district%5B0%5D={}&engine_version=2&maxtarea=60&offer_type=flat&p={}&totime=86400'.format(
-            district,
-            page_n)
-        if page_n > 1:
-            driver.get(init_url)
-        driver.save_screenshot('screen.png')
-        tbody = driver.find_elements_by_tag_name('tbody')[1]
-        # for tbody in tbodies:
-        rows = tbody.find_elements_by_tag_name('tr')
-        rows = [row for row in rows if row.get_attribute('id').split('_')[0] == 'offer']
-        res = pd.DataFrame(map(parse_row, rows))
-        # res
-        final_list.append(res)
-    res_final = pd.concat(final_list)
-    res_final.to_pickle('res_%s_%s.pickle' % (district, datetime.datetime.now().strftime('%m%d%H%M%s')))
+                   '&district%5B0%5D={}&engine_version=2&maxtarea=60&offer_type=flat&p=1&totime=86400'.format(district)
+        driver.get(init_url)
+        # if district == districts.index[0]:
+        #     driver.find_element_by_xpath('//*[@id="layout"]/div[3]/div/div[2]/div/div[2]/a[1]').click()
+        result_n = int(
+            driver.find_element_by_class_name('serp-above__count').find_elements_by_tag_name('strong')[0].text)
+        print '%s results' % result_n
+        n_pages = np.ceil(result_n / float(25))
+        for page_n in range(1, int(n_pages) + 1):
+            init_url = 'http://www.cian.ru/cat.php?deal_type=sale' \
+                       '&district%5B0%5D={}&engine_version=2&maxtarea=60&offer_type=flat&p={}&totime=86400'.format(
+                district,
+                page_n)
+            if page_n > 1:
+                driver.get(init_url)
+            driver.save_screenshot('screen.png')
+            tbody = driver.find_elements_by_tag_name('tbody')[1]
+            if len(tbody.text) == 0:
+                driver.find_element_by_xpath('//*[@id="layout"]/div[3]/div/div[2]/div/div[2]/a[1]').click()
+                tbody = driver.find_elements_by_tag_name('tbody')[1]
+                driver.save_screenshot('screen.png')
+            # for tbody in tbodies:
+            rows = tbody.find_elements_by_tag_name('tr')
+            rows = [row for row in rows if row.get_attribute('id').split('_')[0] == 'offer']
+            res = pd.DataFrame(map(parse_row, rows))
+            # res
+            final_list.append(res)
+        res_final = pd.concat(final_list)
+        res_final.to_pickle('res_%s_%s.pickle' % (district, datetime.datetime.now().strftime('%m%d%H%M%s')))
+        return True
+    except:
+        print ("Unexpected error:", sys.exc_info()[0])
+        return False
 
 def main(districts=districts):
     driver = makePhantomJS()
     for district, name in districts.iteritems():
         processed = False
         while not processed:
-            try:
-                process_district(driver, district, name)
-            except:
-                driver = makePhantomJS()
-                process_district(driver, district, name)
-            else:
-                processed = True
+            processed = process_district(driver, district, name)
+
 
 def makePhantomJS():
     return selenium.webdriver.PhantomJS()
