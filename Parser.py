c87b0d1957b928e0325e47d5d67acba7bcd7d0f7
adima <firtray@gmail.com>
2016-07-25 23:53:07 +0300

    corrected parser

--- Parser.py
+++ Parser.py
@@ -3,101 +3,12 @@ import datetime
 import logging
 import logging.handlers
 
-from transliterate import translit
+
 
 from Main import logger, fh, ch, formatter, mainConc
 from Reference import districts
 from bs4 import BeautifulSoup
 
-logger.setLevel(logging.DEBUG)
-fh.setFormatter(formatter)
-fh.setLevel(logging.DEBUG)
-ch.setFormatter(formatter)
-ch.setLevel(logging.DEBUG)
-logger.addHandler(ch)
-logger.addHandler(fh)
-
-
-def parse_row(row):
-    row_dict = {}
-    row_dict['offer_id'] = row.get_attribute('oid')
-    # print row_dict['offer_id']
-    row_dict['datetime_add'] = datetime.datetime.now()
-    columns = ['objects_item_info_col_%s' % n for n in range(1, 10)]
-    columns.remove('objects_item_info_col_8')
-    for col in columns:
-        col_el = row.find('td', class_=col)
-        if col == 'objects_item_info_col_1':
-            col_divs = col_el.find_all('div')
-            col_as = col_el.find_all('a')
-            row_dict['lat'], row_dict['lng'] = \
-                col_divs[0].find('input').get('value').split(',')
-            try:
-                row_dict['metro_name'] = col_divs[1].find('a').getText()
-                row_dict['metro_distance_lab'] = col_divs[1].find('span')[1].getText()
-            except:
-                pass
-            row_dict['city'] = col_as[1].getText()
-            row_dict['district'] = col_as[2].getText()
-            row_dict['address'] = ' '.join(map(lambda x: x.getText(), col_as[3:]))
-        elif col[-1] == '2':
-            row_dict['room_number_lab'] = col_el.find('a').getText()
-        elif col[-1] == '3':
-            for col_it in col_el.getText().split('\n'):
-                if len(col_it) > 0:
-                    col_it_split = col_it.split(':')
-                    split_first = col_it_split[0]
-                    if split_first == u'Общая':
-                        row_dict['area_overall_raw'] = col_it_split[1]
-                    elif split_first == u'Кухня':
-                        row_dict['area_kitchen_raw'] = col_it_split[1]
-                    elif split_first == u'Жилая':
-                        row_dict['area_living_raw'] = col_it_split[1]
-                    else:
-                        row_dict['area_rooms_raw'] = col_it_split[0]
-        elif col[-1] == '4':
-            get_digit = lambda x: int(''.join([el for el in x if el.isdigit()]))
-            row_dict['price_rub'] = get_digit(col_el.find('div', class_='objects_item_price').strong.getText())
-            row_dict['price_dollars'] = get_digit(col_el.find('div', class_='objects_item_second_price').getText())
-            row_dict['price_square_meter_r'] = get_digit(col_el.find('div', style='color:green;').getText())
-        elif col[-1] == '5':
-            split = col_el.getText().split('\n')
-            split = map(lambda x: x.replace(' ', ''), split)
-            split = [x for x in split if len(x) > 0]
-            row_dict['floor_raw'] = split[0]
-            if len(split) > 1:
-                row_dict['house_type'] = split[1]
-        elif col[-1] == '6':
-            tds = col_el.find_all('td')
-            tds = map(lambda x: x.get_text(strip=True).replace('\n', '').replace(' ', ''), tds)
-            for item in tds:
-                item_split = item.split(':')
-                if len(item_split) > 1:
-                    key = 'addinfo_' + translit(item_split[0], reversed=True).lower().replace(' ', '_')
-                    row_dict[key] = ':'.join(item_split[1:])
-                else:
-                    item_val = item_split[0]
-                    if item_val == u'Новостройка':
-                        row_dict['novostroi'] = 1
-                    elif item_val == u'Вторичка':
-                        row_dict['vtorichka'] = 1
-                    elif item_val == u'Свободная':
-                        row_dict['deal_free'] = 1
-                    elif item_val == u'Альтернатива':
-                        row_dict['deal_alt'] = 1
-        elif col[-1] == '7':
-            row_dict['telephone'] = col_el..getText(strip=True)
-
-        elif col[-1] == '9':
-            row_dict['pay_status'] =\
-                col_el.find('a',
-                        class_ = 'c-iconed c-iconed_m objects_item_payment_status_link_paid').get_text(strip=True)
-            row_dict['date_raw'] = col_el.find('span', class_='objects_item_dt_added').get_text(strip=True)
-            row_dict['seller_id'] = [a for a in col_el.find_all('a') if 'cat.php?id_user' in a.get('href')][0].get_text()
-            row_dict['seller_status'] = col_el.find('span', class_='objects_item_realtor_checked_text').get_text(strip=True)
-            row_dict['ad_text'] = col_el.find('div', class_='objects_item_info_col_comment_text no-truncate').get_text(strip=True)
-            row_dict['ad_href'] = col_el.find('a', class_='objects_item_info_col_card_link no-mobile').get('href')
-    return row_dict
 
 
 if __name__ == '__main__':
